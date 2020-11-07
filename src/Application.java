be782f21960760bd8b29da803c51b1a5bb771daa
Andrey Khrapkov <andrey.i.khrapkov@gmail.com>
2020-11-07 19:02:38 +0300

    added file concatenation and saving result

--- src/Application.java
+++ src/Application.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Application {
     final static String EXT = ".txt";
     final static String DEFAULT_TEST_FOLDER = "testFolders";
+    final static String RESULT_FILE_NAME = "resultFile.txt";
 
     public static void main(String... args){
         Path path = Paths.get(args.length != 0 ? args[0] : DEFAULT_TEST_FOLDER);
@@ -17,14 +18,31 @@ public class Application {
         try {
             files = getTextFileListFrom(path, EXT);
         } catch (IOException e) {
-            System.err.println("Directory not found");
+            System.out.println("Directory not found");
+            System.exit(1);
         }
 
         files.sort((p1,p2) -> p1.getFileName().compareTo(p2.getFileName()));
         files.forEach(v -> { System.out.println(v.getFileName() + " -> " + v); });
 
+        concatenateFilesContent(files, RESULT_FILE_NAME);
     }
 
+    private static void concatenateFilesContent(List<Path> files, String resultFileName) {
+        Path resultFile = Paths.get("./resultFile.txt");
+        try( OutputStream out = Files.newOutputStream(resultFile, StandardOpenOption.CREATE)) {
+            files.forEach(v -> {
+                try (InputStream in = Files.newInputStream(v, StandardOpenOption.READ)) {
+                    in.transferTo(out);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("failed to create result file.");
+            System.exit(1);
+        }
+    }
 
     static List<Path> getTextFileListFrom(Path folder, String ext) throws IOException {
         List<Path> files = new ArrayList<>();
